9ffe298fa2296c40015d03d0b22a9f3b0eefa1a9
Peter Asirvatham <peter.asirvatham@borngroup.com>
2017-03-31 16:02:37 +0530

    Initial commit

---
+++ CoreJava/src/com/pa/ex1/Identifiers.java
@@ -0,0 +1,20 @@
+package com.pa.ex1;
+
+public class Identifiers {
+	
+	public static void main(String[] arg) {
+		System.out.println("Integer Min Value :"+  Integer.MIN_VALUE);
+		System.out.println("Integer Max Value : " +Integer.MAX_VALUE);
+		System.out.println("Long Min Value : " +Long.MIN_VALUE);
+		System.out.println("Long Max Value : " +Long.MAX_VALUE);
+		System.out.println("Float Min Value : " +Float.MIN_VALUE);
+		System.out.println("Float Max Value : " +Float.MAX_VALUE);
+		System.out.println("Double Min Value : " +Double.MIN_VALUE);
+		System.out.println("Double Max Value : " +Double.MAX_VALUE);
+		System.out.println("Short Min Value : " +Short.MIN_VALUE);
+		System.out.println("Short Max Value : " +Short.MAX_VALUE);
+		System.out.println("Byte Min Value : " +Byte.MIN_VALUE);
+		System.out.println("Byte Max Value : " +Byte.MAX_VALUE);
+	}
+
+}
